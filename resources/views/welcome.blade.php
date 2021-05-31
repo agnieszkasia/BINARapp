6668dac531e92c1ab97bd46598a037cd3a252042
agnieszkasia <agnieszkasia.glinka@gmail.com>
2021-05-31 14:57:43 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+    <body class=" bg-dark">
+    <div class="justify-content-center">
+        <div class="h3 text-white text-center mt-4">
+            Generuj plik JPK
+
+        </div>
+    </div>
+
+    </body>
+</html>
