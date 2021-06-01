1f3063f84a7a11c55f3de24db4f7402f6df450c7
agnieszkasia <agnieszkasia.glinka@gmail.com>
2021-06-01 18:03:47 +0200

    Show invoices

--- app/Http/Controllers/TaxSettlementController.php
+++ app/Http/Controllers/TaxSettlementController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller;
 use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
+use function Psy\sh;
 
 
 class TaxSettlementController extends Controller{
@@ -21,11 +22,14 @@ class TaxSettlementController extends Controller{
 
             $reader->open($filepath);
 
+
             foreach ($reader->getSheetIterator() as $sheet) {
                 $j = 0;
                 $values = null;
 
+
                 foreach ($sheet->getRowIterator() as $row) {
+
                     $cells = $row->getCells();
                     foreach ($cells as $cell) {
                         $cell = $cell->getValue();
@@ -37,29 +41,77 @@ class TaxSettlementController extends Controller{
 
                 }
 
+
                 $invoices[$i]['issue_date'] = $values[1][2];
                 $invoices[$i]['due_date'] = $values[2][2];
                 $invoices[$i]['invoice_number'] = $values[5][1] . $values[5][2];
                 $invoices[$i]['company'] = $values[8][0];
-                $invoices[$i]['address'] = $values[10][0] . " " . $values[11][0];
-                $invoices[$i]['NIP'] = $values[12][0];
-                $invoices[$i]['product'] = $values[17][8];
-                $invoices[$i]['service'] = $values[18][8];
-                $invoices[$i]['netto'] = $values[29][1];
-                $invoices[$i]['vat'] = $values[29][2];
-                $invoices[$i]['brutto'] = $values[29][3];
 
+                $row= 0;
+                if (isset($values[12+$row][0])) {
+                    if (str_contains($values[12+$row][0], 'Forma')) {
+
+                        $invoices[$i]['address'] = $values[10+$row][0];
+                        if (isset($values[11][0])){
+                            $invoices[$i]['NIP'] = $values[11+$row][0];
+                        }
+                        else $invoices[$i]['NIP'] = 'brak';
+                    } else {
+                        $invoices[$i]['address'] = $values[10+$row][0] . " " . $values[11+$row][0];
+                        $invoices[$i]['NIP'] = $values[12+$row][0];
+                    }
+                    $invoices[$i]['NIP'] = str_replace(["NIP", ":", " ", "-", ","],"",$invoices[$i]['NIP']);
+                } else{
+                    $invoices[$i]['address'] = 'brak';
+                    $invoices[$i]['NIP'] = 'brak';
+                }
+
+
+                if ($values[14+$row][0] == 'LP') $row = $row -1;
+                elseif ($values[16+$row][0] == 'LP') $row = $row + 1;
+                elseif ($values[17+$row][0] == 'LP') $row = $row +2;
+
+                $invoices[$i]['product'] = $values[17+$row][8];
+
+                if(isset($values[18+$row][8])) {
+                    $invoices[$i]['product2'] = $values[18 + $row][8];
+                } else $row = $row-1;
+
+                if (isset($values[19+$row][8])) {
+                    $invoices[$i]['product3'] = $values[19+$row][8];
+                    $row = $row+1;
+                }
+
+                if (isset($values[20+$row][8])) {
+                    $invoices[$i]['product3'] = $values[20+$row][8];
+                    $row = $row+1;
+                }
+
+                if (isset($values[21+$row][8])) {
+                    $invoices[$i]['product3'] = $values[21+$row][8];
+                    $row = $row+1;
+                }
+
+                if (isset($values[29+$row][0])) $invoices[$i]['netto'] = $values[29+$row][0];
+                else $invoices[$i]['netto'] = 'brak';
+                if (isset($values[29+$row][2]))$invoices[$i]['vat'] = $values[29+$row][2];
+            else $invoices[$i]['vat'] = 'brak';
+                if (isset($values[29+$row][3]))$invoices[$i]['brutto'] = $values[29+$row][3];
+            else $invoices[$i]['brutto'] = 'brak';
 
             }
+
             $reader->close();
             $i++;
         }
+
+//        dd($values);
 
 //        generateDailySalesStatementFile();
 //        generateCSVFile();
 //        generateXMLfile();
 
-        return view('showInvoices', compact($invoice));
+        return view('show_invoices', compact('invoices'));
     }
 
 
---
+++ resources/views/show_invoices.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+        <script src="https://unpkg.com/read-excel-file@4.x/bundle/read-excel-file.min.js"></script>
+
+    </head>
+    <body class="bg-secondary">
+    <div class="">
+        <div class=" ">
+            <div class="h3 text-white text-center mt-4 col-12">
+                FAKTURY
+
+            </div>
+            <div class="position-absolute justify-content-end col-4 ri">
+                <button>Pobierz CSV</button>
+            </div>
+        </div>
+
+
+        <table class="table-responsive table table-light">
+            <thead class="thead-dark">
+                <tr >
+                    <th scope="col">Data wystawienia</th>
+                    <th scope="col">Data sprzedaży</th>
+                    <th scope="col">Numer faktury</th>
+                    <th scope="col">Nabywca</th>
+                    <th scope="col">Adres</th>
+                    <th scope="col">NIP</th>
+                    <th scope="col">Produkty</th>
+                    <th scope="col">Liczba wierszy faktury</th>
+                    <th scope="col">Wartości produktów</th>
+                    <th scope="col">Netto</th>
+                    <th scope="col">VAT</th>
+                    <th scope="col">Brutto</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach($invoices as $invoice)
+                <tr>
+                    <td>{{$invoice['issue_date']}}</td>
+                    <td>{{$invoice['due_date']}}</td>
+                    <td>{{$invoice['invoice_number']}}</td>
+                    <td>{{$invoice['company']}}</td>
+                    <td>{{$invoice['address']}}</td>
+                    <td>{{$invoice['NIP']}}</td>
+                    <td>{{$invoice['product']}}</td>
+                    <td></td>
+                    <td>{{$invoice['issue_date']}}</td>
+                    <td>{{$invoice['netto']}}</td>
+                    <td>{{$invoice['vat']}}</td>
+                    <td>{{$invoice['brutto']}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+
+    </div>
+
+    </body>
+</html>
