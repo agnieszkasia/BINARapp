7e2438a797535b0f9817f5cd6704c91cde71a6cd
agnieszkasia <agnieszkasia.glinka@gmail.com>
2021-06-28 18:26:50 +0200

    Change welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,21 +17,18 @@
         <script src="https://unpkg.com/read-excel-file@4.x/bundle/read-excel-file.min.js"></script>
 
     </head>
-    <body class="bg-gray">
-    <div class="justify-content-center">
-        <form action="{{route('send')}}" method="POST" enctype="multipart/form-data">
-        @csrf
+    <body class="bg-black m-auto">
+    <div class="justify-content-center vh-100 d-flex justify-content-center">
+            <div class="pt-3 m-auto">
+                <div class="m-auto display-1 text-warning mb-3">
+                    BINAR<span class="text-white-50">app</span>
+                </div>
 
-            <div class="h3 text-white bg-black text-center pt-4 mb-0 pb-3">
-                GENERUJ PLIK JPK
-
-                <div class="d-flex float-right justify-content-end mx-5">
-                    <a type="submit" class="btn btn-next " href="{{ url('/add_files') }}">Dalej</a>
+                <div class="text-center">
+                    <a type="submit" class="btn btn-next fw-bold " href="{{ url('/add_files') }}">DODAJ FAKTURY</a>
                 </div>
 
             </div>
-        </form>
-
     </div>
 
     </body>
